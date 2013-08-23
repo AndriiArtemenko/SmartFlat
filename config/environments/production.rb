68d62587f9452fe78deab77f552d75c708297361
AndriiArtemenko <andrey.to.ims@gmail.com>
2013-08-23 22:49:41 +0300

    Change session logout method

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,4 +64,5 @@ SmartFlat::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+  config.sign_out_via = :delete
 end
