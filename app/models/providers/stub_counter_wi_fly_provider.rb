26320169694d5afc0915ba473575788f9bdeb860
AndriiArtemenko <andrey.to.ims@gmail.com>
2013-09-16 13:40:00 +0300

    Added Stub provider for meter's

---
+++ app/models/providers/stub_counter_wi_fly_provider.rb
@@ -0,0 +1,12 @@
+class StubCounterWiFlyProvider < CounterWiFlyProvider
+
+  # Return a counter value.
+  def get_value
+    return 0
+  end
+
+  # Return a counter value.
+  def reset()
+  end
+
+end
